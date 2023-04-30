01a01b659fac88741c4bd68a3215c0e6a0aa9c08
iqbalfachry19 <ifachry@rocketmail.com>
2023-05-01 01:50:01 +0700

    new

---
+++ src/routes/Nested.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let answer: number;
+	let string = 'this string contains some <strong>HTML!</strong>';
+	let count = 0;
+	let numbers = [1, 2, 3, 4];
+
+	$: doubled = count * 2;
+
+	$: if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 0;
+	}
+	function increment() {
+		count += 1;
+	}
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+		numbers = numbers;
+	}
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<p>{@html string}</p>
+<button on:click={increment}>Clicked {count} {count === 1 ? 'time' : 'times'}</button>
+<p>{count} doubled is {doubled}</p>
+<p>{numbers.join('+')}= {sum}</p>
+<button on:click={addNumber}>Add a number</button>
+<p>The answer is {answer}</p>
---
+++ src/routes/PackageInfo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let name: string;
+	export let version: number;
+	export let speed: string;
+	export let website: string;
+
+	$: href = `https://www.npmjs.com/package/${name}`;
+</script>
+
+<p>
+	The <code>{name}</code> package is {speed} fast. Download version {version}
+	from
+	<a {href}>npm</a>
+	and <a href={website}>learn more here</a>
+</p>
+
+<style>
+	code {
+		font-family: Menlo;
+		padding: 0.1em 0.2em;
+		border-radius: 0.2em;
+	}
+</style>
